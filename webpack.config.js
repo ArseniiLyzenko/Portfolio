bb62827feb6a5222d209809ec1aba9cb2d973a35
lyzard <arseniismart@gmail.com>
2022-05-24 20:23:31 -0400

    Updated: content

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: ["./src/scripts/index.js"],
   mode: 'production',
+  watch: true,
   devServer: {
     contentBase: "./dist"
   },
